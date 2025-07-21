2c59fd454534d874075866206a89e8be96bc07b2
unknown <mukungapriviledge@gmail.com>
2025-07-21 09:49:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Characters from "./components/Characters"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+
+const App = () => {
+  return (
+    <div className=''>
+      <Header/>
+      <Hero/>
+      <Characters/>
+    </div>
+  )
+}
+
+export default App
